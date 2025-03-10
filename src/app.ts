e69b3fc7a182a088d8ce1bffdedb91f3c0855e7c
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-03-10 09:54:10 +0330

    feat: add envs

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import express from 'express'
+import { config } from "dotenv"
+
+config()
 
 const app = express()
 
@@ -7,6 +10,6 @@ app.get('/', (req, res) => {
     res.json({ message: 'hello world' })
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
     console.log('App running on port 3000')
 })
