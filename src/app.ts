bc9d6e4a6876fd676643557e551ede4a475c4682
sajad-ahmadnzhad <ahmadnzhad22@gmail.com>
2025-03-14 15:51:26 +0330

    feat: add get data api for test

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,16 @@ config();
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'hello world' });
+    res.json({ message: 'hello world' });
 });
 
+app.get("/data", (req, res) => {
+    res.json({
+        data: [{ id: 1 }]
+    })
+})
+
+
 app.listen(process.env.PORT, () => {
-  console.log('App running on port 3000');
+    console.log('App running on port 3000');
 });
